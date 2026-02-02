80db03b47ffc50220df61924635f2e29975c3925
parth <pgpatel018@gmail.com>
2026-02-02 11:45:58 +0530

    test

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,5 +1,5 @@
 const config = {
-	apiUrl: "http://localhost:1245" //has to be changed in production 
+	apiUrl: "https://ctf-backend-parth-pixelque-dev.apps.rm1.0a51.p1.openshiftapps.com" //has to be changed in production 
   };
   
   export default config;
